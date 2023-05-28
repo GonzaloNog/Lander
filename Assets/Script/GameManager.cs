01849dd2d7174bbfb6bd8c1cb077927bbfe13792
gzeissSRL <63429016+GonzaloNog@users.noreply.github.com>
2023-05-28 17:31:54 -0300

    construccion terminada

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -6,8 +6,12 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     public GameObject[] buildings;
+    public UIManager canvas;
+    public int[] rockCost;
+    public int[] woodCost;
     private int buildID = 0;
-
+    public int rock = 0;
+    public int wood = 0;
     private void Awake()
     {
         if (instance == null)
@@ -27,5 +31,13 @@ public class GameManager : MonoBehaviour
     public GameObject getBuilding()
     {
         return buildings[buildID];
+    }
+    public int getRockCost()
+    {
+        return rockCost[buildID];
+    }
+    public int getWoodCost()
+    {
+        return woodCost[buildID];
     }
 }
--- Assets/Script/Tile.cs
+++ Assets/Script/Tile.cs
@@ -6,9 +6,12 @@ public class Tile : MonoBehaviour
 {
     private bool built = false;
     private bool work = false;
+    private bool notMoney = false;
+    private float timeChange = 0.0f;
+    private Color color1;
     void Start()
     {
-        
+        ColorUtility.TryParseHtmlString("#13760C", out color1);
     }
 
     // Update is called once per frame
@@ -20,12 +23,33 @@ public class Tile : MonoBehaviour
             {
                 if (work)
                 {
-                    GameObject temp = Instantiate(GameManager.instance.getBuilding(), this.transform.position,this.transform.rotation);
-                    Vector3 position = new Vector3(this.transform.position.x, this.transform.position.y + (temp.GetComponent<Collider>().bounds.size.y / 2), this.transform.position.z);
-                    temp.transform.position = position;
-                    built = true;
-                    Debug.Log("New Build");
+                    if (GameManager.instance.getRockCost() <= GameManager.instance.rock && GameManager.instance.getWoodCost() <= GameManager.instance.wood)
+                    {
+                        GameManager.instance.wood -= GameManager.instance.getWoodCost();
+                        GameManager.instance.rock -= GameManager.instance.getRockCost();
+                        Quaternion rot = Quaternion.Euler(0, 30, 0);
+                        GameObject temp = Instantiate(GameManager.instance.getBuilding(), this.transform.position, rot);
+                        Vector3 position = new Vector3(this.transform.position.x, this.transform.position.y + (temp.GetComponent<Collider>().bounds.size.y / 2), this.transform.position.z);
+                        temp.transform.position = position;
+                        built = true;
+                        Debug.Log("New Build");
+                    }
+                    else
+                    {
+                        gameObject.GetComponent<Renderer>().material.color = new Color(0.78f, 0, 0, 1);
+                        notMoney = true;
+                        timeChange = 0.0f;
+                    }
                 }
+            }
+        }
+        if (notMoney)
+        {
+            timeChange += Time.deltaTime;
+            if(timeChange >= 0.2f)
+            {
+                gameObject.GetComponent<Renderer>().material.color = color1;
+                notMoney = false;
             }
         }
     }
